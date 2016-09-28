108e369d232a9bee092d9f61a8f52d5a56f3dd7b
jasmin47 <alxsanborn@gmail.com>
2016-09-28 17:47:22 -0400

    Abi @abidiaz, Alex @alxsanborn, Ece @ecezalp, Veronica @vronnieli

---
+++ src/components/cocktails_index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router'
+
+function CocktailsIndex (props) {
+  return (
+  <div>
+    <ul>
+      {props.cocktails.map(cocktail => <Link to={`/cocktails/${cocktail.id}`}><li key={cocktail.id}>{cocktail.name}</li></Link>)}
+      {props.children}
+
+    </ul>
+  </div>)
+};
+
+function mapStateToProps(state){
+  return {
+    cocktails: state.cocktails
+  }
+}
+
+
+const componentCreator = connect(mapStateToProps)
+export default componentCreator(CocktailsIndex);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {Router, Route, browserHistory} from 'react-router';
+import routes from './routes';
+import { Provider } from 'react-redux'
+
+import rootReducer from './reducers/index';
+
+import { fetchCocktails } from './actions';
+
+const store = createStore(rootReducer)
+
+//store.dispatch (fetchCocktails());
+
+ReactDOM.render(
+
+<Provider store={store} >
+    <Router history={browserHistory} routes={routes} />
+  </Provider>, document.getElementById('container')
+)
